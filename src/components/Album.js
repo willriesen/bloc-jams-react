71bcaf065c58a850164517651022cc1119a352ca
Will Riesen <williamdriesen@Williams-MacBook-Pro.local>
2019-03-07 11:11:19 -0600

    album completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 const Album = () => (
@@ -8,14 +7,3 @@ const Album = () => (
 );
 
 export default Album;
-=======
-import React from 'react';
-
-const Album = () => (
-  <section className="album">
-    Album will go here
-  </section>
-);
-
-export default Album;
->>>>>>> dad0e775e1faa75d194b67871f702d3971146920
