90a1212e2bed416c4cda0f749ebb710fac257798
Will Riesen <williamdriesen@williams-mbp.attlocal.net>
2019-03-13 16:12:30 -0500

    audio-playback-issue

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,15 +10,49 @@ class Album extends Component {
      });
 
      this.state = {
-       album: album
+       album: album,
+       currentSong: album.songs[0],
+       isPlaying: false
      };
-   }
+   
+     this.audioElement = document.createElement('audio');
+     this.audioElement.src = album.songs[0].audioSrc;
+    }
 
+    play() {
+      this.audioElement.play();
+      this.setState({ isPlaying: true });
+    }
+
+    pause() {
+      this.audioElement.pause();
+      this.setState({ isPlaying: false });
+    }
+
+    setSong(song) {
+      this.audioElement.src = song.audioSrc;
+      this.setState({ currentSong: song });
+    }
+
+    handleSongClick(song) {
+      const isSameSong = this.state.currentSong === song;
+      if (this.state.isPlaying && isSameSong) {
+        this.pause();
+      } else {
+        if (!isSameSong) { this.setSong(song)}; }
+        this.play();
+      }
+    }
+
+    thisSongIsPlaying(song) {
+      return this.state.currentSong === song && this.state.isPlaying;
+    }
+    
   render() {
     return (
       <section className="album">
         <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+           <img id="album-cover-art" src={this.state.album.albumCover} />
            <div className="album-details">
            <h1 id="album-title">{this.state.album.title}</h1>
              <h2 className="artist">{this.state.album.artist}</h2>
@@ -33,8 +67,14 @@ class Album extends Component {
            </colgroup>  
            <tbody>
               {this.state.album.songs.map( (song, index) =>
-                 <tr className="song" key={index} onClick={() => this.handleSongClick(song)} onMouseEnter={() => this.onHover(index)} onMouseLeave={() => this.offHover()}>
-                     <td className="song-number">{song.number}</td>
+                 <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                     <td className="song-actions">
+                         <button>
+                             <span className={this.thisSongIsPlaying(song) ? "invisible" : "song-number"}>{index+1}</span>
+                             <span className={this.thisSongIsPlaying(song) ? "invisible" : "ion-play"}></span>
+                             <span className={this.thisSongIsPlaying(song) ? "ion-pause" : "invisible"}></span>
+                         </button>
+                     </td>
                      <td className="song-title">{song.title}</td>
                      <td className="song-duration">{song.duration}</td>
 
