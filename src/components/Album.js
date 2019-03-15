8ed9f30987dd70e8c50f53d7c6cd52a621b7d615
Will Riesen <williamdriesen@Williams-MacBook-Pro.local>
2019-03-15 10:32:18 -0500

    playerbar updated

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar'
 
 class Album extends Component {
    constructor(props) {
@@ -43,6 +44,22 @@ class Album extends Component {
         this.play();
       }
     
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+      }
+    
+      handleNextClick () {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play(newSong);
+    
+      }
     onHover(index) {
         this.setState({ isHovered: index });
     }
@@ -89,6 +106,13 @@ class Album extends Component {
                   </tr>)}
            </tbody>
         </table>
+        <PlayerBar
+           isPlaying={this.state.isPlaying}
+           currentSong={this.state.currentSong}
+           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
+         />
       </section>
     );
   }
